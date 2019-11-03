117bd5c4724323e5b8a71cd501db41a4752f7b10
Jose Luis Castillo Peña <joseluis.castillo@iesdonana.org>
2019-11-03 16:51:31 +0100

    Prueba Heroku2

--- db/segurcastillo.sql
+++ db/segurcastillo.sql
@@ -2,3 +2,10 @@
 -- Archivo de base de datos --
 ------------------------------
 
+DROP TABLE IF EXISTS clientes CASCADE;
+
+CREATE TABLE clientes
+(
+    id            BIGSERIAL     PRIMARY KEY
+  , nombre        varchar(255)       
+);
