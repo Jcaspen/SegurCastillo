64cebd67936d375b445a04cff7dc8189768e1094
Jose Luis Castillo Peña <joseluis.castillo@iesdonana.org>
2019-11-05 17:33:37 +0100

    Empiezo BD y creo alguna vista

---
+++ models/Clientes.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "clientes".
+ *
+ * @property int $id
+ * @property string $nombre
+ */
+class Clientes extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'clientes';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nombre'], 'string', 'max' => 255],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'nombre' => 'Nombre',
+        ];
+    }
+}
--- views/layouts/main.php
+++ views/layouts/main.php
@@ -42,10 +42,10 @@ AppAsset::register($this);
         'options' => ['class' => 'navbar-nav'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
+            ['label' => 'Clientes', 'url' => ['/clientes/index']],
+            ['label' => 'Contacto', 'url' => ['/site/contact']],
             Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
+                ['label' => 'Conectar', 'url' => ['/site/login']]
             ) : (
                 '<li class="nav-item">'
                 . Html::beginForm(['/site/logout'], 'post')
@@ -72,7 +72,7 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="float-left">&copy; My Company <?= date('Y') ?></p>
+        <p class="float-left">&copy; SegurCastillo <?= date('Y') ?></p>
 
         <p class="float-right"><?= Yii::powered() ?></p>
     </div>
