7f084270a4814b3b50cb9f7b8688fbe71ab21d15
Jose Luis Castillo Peña <joseluis.castillo@iesdonana.org>
2020-03-14 19:23:29 +0100

    Página de Login en español.

--- models/LoginForm.php
+++ models/LoginForm.php
@@ -12,9 +12,9 @@ use yii\base\Model;
  */
 class LoginForm extends Model
 {
-    public $username;
-    public $password;
-    public $rememberMe = true;
+    public $usuario;
+    public $contraseña;
+    public $recordarme = true;
 
     private $_user = false;
 
@@ -26,11 +26,11 @@ class LoginForm extends Model
     {
         return [
             // username and password are both required
-            [['username', 'password'], 'required'],
+            [['usuario', 'contraseña'], 'required'],
             // rememberMe must be a boolean value
-            ['rememberMe', 'boolean'],
+            ['recordarme', 'boolean'],
             // password is validated by validatePassword()
-            ['password', 'validatePassword'],
+            ['contraseña', 'validatePassword'],
         ];
     }
 
@@ -46,8 +46,8 @@ class LoginForm extends Model
         if (!$this->hasErrors()) {
             $user = $this->getUser();
 
-            if (!$user || !$user->validatePassword($this->password)) {
-                $this->addError($attribute, 'Incorrect username or password.');
+            if (!$user || !$user->validatePassword($this->contraseña)) {
+                $this->addError($attribute, 'Usuario o contraseña incorrecta.');
             }
         }
     }
@@ -59,7 +59,7 @@ class LoginForm extends Model
     public function login()
     {
         if ($this->validate()) {
-            return Yii::$app->user->login($this->getUser(), $this->rememberMe ? 3600 * 24 * 30 : 0);
+            return Yii::$app->user->login($this->getUser(), $this->recordarme ? 3600 * 24 * 30 : 0);
         }
         return false;
     }
@@ -72,7 +72,7 @@ class LoginForm extends Model
     public function getUser()
     {
         if ($this->_user === false) {
-            $this->_user = Usuarios::findOne(['login' => $this->username]);
+            $this->_user = Usuarios::findOne(['login' => $this->usuario]);
         }
 
         return $this->_user;
--- views/site/login.php
+++ views/site/login.php
@@ -7,13 +7,13 @@
 use yii\helpers\Html;
 use yii\bootstrap4\ActiveForm;
 
-$this->title = 'Login';
+$this->title = 'Acceso';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-login">
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please fill out the following fields to login:</p>
+    <p>Bienvenido al Portal SegurCastillo, introduce tu usuario y contraseña:</p>
 
     <?php $form = ActiveForm::begin([
         'id' => 'login-form',
@@ -23,12 +23,12 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]); ?>
 
-        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+        <?= $form->field($model, 'usuario')->textInput(['autofocus' => true]) ?>
 
-        <?= $form->field($model, 'password')->passwordInput() ?>
+        <?= $form->field($model, 'contraseña')->passwordInput() ?>
 
-        <?= $form->field($model, 'rememberMe')->checkbox() ?>
-    
+        <?= $form->field($model, 'recordarme')->checkbox() ?>
+
         <div class="form-group">
             <div class="offset-sm-2">
                 <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
@@ -37,8 +37,4 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <?php ActiveForm::end(); ?>
 
-    <div class="offset-sm-2" style="color:#999;">
-        You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
-        To modify the username/password, please check out the code <code>app\models\User::$users</code>.
-    </div>
 </div>
