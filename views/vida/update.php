c943f10d5961a94797eb66b0baaab3265dedc4b0
Jose Luis Castillo Peña <joseluis.castillo@iesdonana.org>
2020-05-15 11:01:52 +0200

    vista modificar vida

--- views/vida/update.php
+++ views/vida/update.php
@@ -1,21 +1,45 @@
 <?php
 
-use yii\helpers\Html;
+use yii\bootstrap4\Html;
+use yii\bootstrap4\ActiveForm;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Vida */
 
-$this->title = 'Update Vida: ' . $model->id;
+$this->title = 'Modificar Vida: ' . $model->tomador_dni;
 $this->params['breadcrumbs'][] = ['label' => 'Vidas', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
+$this->params['breadcrumbs'][] = 'Modificar Vida';
 ?>
 <div class="vida-update">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <div class="vida-form">
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'tomador_dni')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'ocupacion')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'ingresos_anuales')->textInput(['maxlength' => true]) ?>
+
+        <?= $form->field($model, 'tipo_poliza')->dropdownList(['Vida','Plan de Pensiones', 'Salud']) ?>
+
+        <?= $form->field($model, 'ingreso_mensual')->textInput() ?>
+
+        <?= $form->field($model, 'capital')->textInput() ?>
+
+        <?= $form->field($model, 'cuestionario')->textInput() ?>
+
+        <?= $form->field($model, 'prima')->textInput() ?>
+
+        <div class="form-group">
+            <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
 
 </div>
